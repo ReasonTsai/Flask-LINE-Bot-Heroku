52ddf9bdb656ca20326ff9204ae1cc560be46b39
ReasonTsai <76902669+ReasonTsai@users.noreply.github.com>
2021-01-03 20:39:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,9 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     get_message = event.message.text
-
+    user_id = event.source.user_id
+    profile = line_bot_api.get_profile(user_id)
+    print(profile.display_name)
     # Send To Line
-    reply = TextSendMessage(text="阿志還敢尻尻啊")
+    reply = TextSendMessage(text=profile.display_name + "還敢尻尻啊")
     line_bot_api.reply_message(event.reply_token, reply)
